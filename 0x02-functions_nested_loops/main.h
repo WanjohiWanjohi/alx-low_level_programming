975b3db7924cd6b68b4fd84698ecc32608204906
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-04-12 12:19:32 -0700

    Starting on Loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+/**
+ * *File:main.h
+ * *Author:Wamuyu Wanjohi
+ * *Desc:header file containing functions declaration
+ * */
+
+void print_zero(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _putchar(char);
